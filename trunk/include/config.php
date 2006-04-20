6b4dec8dc28dfe44360ab712797375d4fc240232
dotslash <dotslash@9ac7effd-5938-0410-9999-b3eb7259264e>
2006-04-20 15:24:53 +0000

    - berlios database, svn mailing-list test

--- trunk/include/config.php
+++ trunk/include/config.php
@@ -2,10 +2,10 @@
 	error_reporting(E_ALL);
 
 	/// Datenbankeinstellungen
-	$db_config['user'] = "siwu";
-	$db_config['pass'] = "test";
+	$db_config['user'] = "liman";
+	$db_config['pass'] = "drownamil";
 	$db_config['name'] = "liman";
-	$db_config['host'] = "mysql";
+	$db_config['host'] = "db.berlios.de";
 	$db_config['prefix'] = "liman_";
 
 	$ext = "xhtml";	// Endung der von den Usern abrufbaren Dateien
