74ef531ffa66467d6e7b1abe15759daefe5790f0
lazhur <lazhur@9ac7effd-5938-0410-9999-b3eb7259264e>
2006-05-25 12:08:46 +0000

    - GetAll zu Autor hinzugefügt (UML+Quelltext) - Autor dokumentiert

--- trunk/include/autor.php
+++ trunk/include/autor.php
@@ -5,7 +5,8 @@ if(!defined("Autor"))
 
 	/*! \brief Verwaltet Autoren
 	 *
-	 *  TODO
+	 *  Stellt Funktionen zum Hinzufügen von Autoren und Bereinigen nicht
+	 *  mehr benutzter Autoren bereit.
 	 *  \pre Datenbankverbindung muss bestehen
 	 */
 	class Autor
@@ -15,7 +16,8 @@ if(!defined("Autor"))
 		
 		/*! \brief Legt Autorenobjekt an
 		 *
-		 *  TODO
+		 *  Legt ein neues Autorenobjekt aus einem Objekt mit den
+		 *  Attributen Nr und Name an.
 		 *  \pre -
 		 *  \param[in] $data Objekt mit Autorendaten der Form
 		 *    - Nr
@@ -27,19 +29,44 @@ if(!defined("Autor"))
 
 		/*! \brief Entfernt unnötige Autoren
 		 *
-		 *  TODO
+		 *  Entfernt aus Autoren alle Autoren, die keine Verbindung
+		 *  (Autoren_Literatur-Tabelle) mehr mit Literatur haben.
 		 *  \pre Datenbankverbindung muss bestehen
 		 */
 		function Clean()
 		{
 		}
 
+		/*! \brief Rückgabe eines Feldes von Autoren
+		 *
+		 *  Liest alle Autoren die einer Literatur ($nr) zugeordnet
+		 *  sind aus Bibliothek aus und gibt sie als Feld des Typs
+		 *  Autor zurück.
+		 *  \param[in] $nr Nr einer Literatur mit Autoren
+		 *  \pre Datenbankverbindung muss bestehen
+		 *  \return Feld vom Typ Autor
+		 */
+		function GetAll($nr)
+		{
+			/// \todo implementieren
+			$authors = array();
+
+			//return $authors;
+		}
+
 		/*! \brief Legt neue Autoren aus kommagetrennter Liste an
 		 *
-		 *  TODO
+		 *  Teilt die kommagetrennte Liste von Autoren in einzelne
+		 *  Autorennamen ein und überprüft gegen Autorentabelle, ob
+		 *  diese schon in der Datenbank existieren. Sollten diese
+		 *  schon existieren, dann wird Autor_Nr ausgelesen und zum
+		 *  Feld hinzugefügt. Sollten diese noch nicht existieren,
+		 *  werden sie hinzugefügt und die neuen Autor_Nr zum Feld
+		 *  hinzugefügt. Das komplette Feld vom Typ Autor_Nr wird
+		 *  zurückgegeben.
 		 *  \pre Datenbankverbindung muss bestehen
 		 *  \param[in] $autoren String mit kommagetrennter Liste von Autoren
-		 *  \return Datenbankverbindung muss bestehen
+		 *  \return Feld mit Autor_Nr der Autoren
 		 */
 		function Split($autoren)
 		{
