401898d9ce692c08dbcf42efa579149fa285bc1f
lazhur <lazhur@9ac7effd-5938-0410-9999-b3eb7259264e>
2006-06-13 19:00:04 +0000

    vertauschte config.php zurückgetauscht

--- trunk/docs/beleg3/sourcecodes/include/config.php
+++ trunk/docs/beleg3/sourcecodes/include/config.php
@@ -1,10 +1,12 @@
 <?php
+	error_reporting(E_ALL);
+
 	// Datenbankeinstellungen
 	$db_config['dbms'] = "mysql";	// welche Datenbankklasse benutzen?
-	$db_config['user'] = "liman"; // Nutzername des Datenbanknutzers
-	$db_config['pass'] = "drownamil"; // Passwort für Datenbanknutzer
-	$db_config['name'] = "liman"; // Name der Datenbank
-	$db_config['host'] = "db.berlios.de"; // Adresse des Datenbankservers
+	$db_config['user'] = ""; // Nutzername des Datenbanknutzers
+	$db_config['pass'] = ""; // Passwort für Datenbanknutzer
+	$db_config['name'] = ""; // Name der Datenbank
+	$db_config['host'] = ""; // Adresse des Datenbankservers
 	$db_config['prefix'] = "liman_"; // Prefix der Tabellennamen
 
 	$ext = "php";	// Endung der von den Usern abrufbaren Dateien
